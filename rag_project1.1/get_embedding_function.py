ae99e6a5eda54d144054f6a83d3ef3fa7ed15177
faruk501 <ferekf-romerop@unilibre.edu.co>
2025-09-05 18:40:44 -0500

    Change Google API key in get_embedding_function.py

--- rag_project1.1/get_embedding_function.py
+++ rag_project1.1/get_embedding_function.py
@@ -1,7 +1,7 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 #  API key de Google
-GOOGLE_API_KEY = "AIzaSyCw9VzUFhWWadmurQ47Qz-6p4JzaF6zGhU"
+GOOGLE_API_KEY = "AIzaSyAGartUrEQ36t4CkmI1lZVvMragPuxcwkE"
 
 # Qdrant Cloud
 QDRANT_URL = "https://6b3a9368-b08f-4d67-a6d3-3aa89b0ba118.europe-west3-0.gcp.cloud.qdrant.io"
